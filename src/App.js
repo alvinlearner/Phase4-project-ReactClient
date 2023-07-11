8cb44af7786d9feb6e261f938547d7b8f9469025
alvinkenyagah <alvin47k@outlook.com>
2023-07-11 17:47:49 +0300

    Update session manager

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Route, Routes} from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 import Home from './components/routes/Home';
 import About from './components/routes/About';
@@ -12,7 +12,6 @@ import Organisation from './components/AdminDash/Organisation';
 import DashFooter from './components/AdminDash/DashHome';
 import Statistics from './components/AdminDash/Statistics';
 import Graphs from './components/AdminDash/Graphs';
-
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -29,17 +28,16 @@ function App() {
     localStorage.setItem('isLoggedIn', JSON.stringify(true));
   };
 
-
-
   return (
     <div className="App">
-
-      {isLoggedIn ? (
-        <React.Fragment>
-          <Routes>
-            <Route path="/" element={<LoginForm />} />
-            <Route path="/admin" element={<AdminDash />} />
-            <Route path="/signup" element={<SignupForm />} />
+      <Routes>
+        <Route path="/" element={<LoginForm onLogin={handleLogin} />} />
+        <Route path="/signup" element={<SignupForm />} />
+        {isLoggedIn && (
+          <React.Fragment>
+            
+  	    <Route path="/admin" element={<AdminDash />} />
+            
             <Route path="/home" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/events" element={<Events />} />
@@ -48,17 +46,12 @@ function App() {
             <Route path="/analytics/statistics" element={<Statistics/>}/>
             <Route path="/analytics/graphs" element={<Graphs/>} />
             <Route path='/adminhome' element={<DashFooter/>}/>
-          </Routes>
-        </React.Fragment>
-      ) : (
-        <LoginForm onLogin={handleLogin} />
-      )}
-
+          </React.Fragment>
+        )}
+        <Route path="/*" element={<Navigate to="/" />} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
-
-
-
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,44 +4,23 @@ import { Menuitems } from './Menuitems';
 import { Link, useNavigate } from 'react-router-dom';
 import logo from '../../src/assets/images/logo.png';
 
-function Navbar() {
+function Navbar({ user, setUser }) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [clicked, setClicked] = useState(false); // Added 'clicked' state
   const navigate = useNavigate();
 
-  const handleLogout = () => {
-    setIsLoggedIn(false);
-    localStorage.setItem('isLoggedIn', JSON.stringify(false));
-  
-    // Perform the fetch delete request to delete the user session
-    fetch('/logout', {
-      method: 'DELETE',
+
+
+
+  function handleLogout() {
+    fetch("/logout", { method: "DELETE" }).then((r) => {
+      if (r.ok) {
+        setUser(null);
+      }
     })
-      .then(response => {
-        // Handle the response and perform necessary actions
-        if (response.ok) {
-          // User session deleted successfully, perform any required actions (e.g., redirect to login page)
-          navigate('/');
-  
-          // Replace the current history entry with the login page
-          window.history.replaceState(null, '', '/');
-  
-          // Clear the browser history to prevent going back to the application after logout
-          window.history.go(0);
+    .then((r)=>{navigate('/')})
+  }
 
-          alert('session deleted')
-
-        } else {
-          // An error occurred, handle the error (e.g., display an error message)
-          alert('Failed to delete user session');
-        }
-      })
-      .catch(error => {
-        // Handle any network or fetch-related errors
-        console.log('Error occurred while deleting user session', error);
-      });
-  };
-  
   const handleClick = () => {
     setClicked(!clicked); // Toggle the 'clicked' state
   };
